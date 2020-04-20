9183e68db1b3e11d540d19566b7064742b02ac74
sidmal <dmitriy.sinichkin@gmail.com>
2020-04-20 21:08:32 +0300

    feat: fix unit tests

--- internal/service/order_test.go
+++ internal/service/order_test.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/go-redis/redis"
-	"github.com/gogo/protobuf/proto"
+	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/google/uuid"
 	"github.com/jinzhu/now"
